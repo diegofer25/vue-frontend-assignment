824a8a53dc38b39d04265ca1293bdd24ef73c592
Diego Lamarão <diego.lamarao92@gmail.com>
2019-09-20 09:56:05 -0300

    created company table template

---
+++ src/components/templates/CompanyTable.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="company-table flex column pa-lg">
+    Company Table
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CompanyTable'
+}
+</script>
+
+<style lang="scss" scoped>
+.company-table {
+}
+</style>
