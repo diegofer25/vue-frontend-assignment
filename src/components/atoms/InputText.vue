5a1861112a5ec886dd9f076e46402755ff0e04d6
Diego Lamarão <diego.lamarao92@gmail.com>
2019-09-20 09:54:25 -0300

    created input text

---
+++ src/components/atoms/InputText.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="input-text flex column">
+    <label class="mb-xs" v-if="label" :for="label" v-text="label"></label>
+    <input
+      v-if="type === 'text'"
+      type="text"
+      :value="value"
+      @input="$emit('input', $event.target.value)"
+      :name="label"
+      :placeholder="placeholder"
+    />
+    <textarea
+      v-else
+      :class="{ noresize: !resize }"
+      :name="label"
+      :cols="cols"
+      :rows="rows"
+      :value="value"
+      @input="$emit('input', $event.target.value)"
+      :placeholder="placeholder"
+    ></textarea>
+  </div>
+</template>
+
+<script>
+import { input } from 'Components/atoms'
+export default {
+  name: 'InputText',
+  props: {
+    value: {
+      type: String,
+      default: ''
+    },
+    type: {
+      type: String,
+      required: true,
+      validator: (value) => {
+        return ['text', 'textarea'].indexOf(value) !== -1
+      }
+    },
+    label: {
+      type: String,
+      default: ''
+    },
+    placeholder: {
+      type: String,
+      default: ''
+    },
+    rows: {
+      type: String,
+      default: '1'
+    },
+    cols: {
+      type: String,
+      default: '1'
+    },
+    resize: {
+      type: Boolean,
+      default: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.input-text {
+  min-width: 100%;
+  label {
+    text-transform: uppercase;
+    color: $grey;color: $grey;
+    font-size: 13px;
+  }
+  input {
+    height: 30px;
+  }
+  input, textarea {
+    border: none;
+    border-radius: 3px;
+    box-shadow:  0 0 5px rgb(199, 199, 199), 0 0 5px rgb(255, 255, 255);
+    text-indent: 10px;
+    &::placeholder {
+      opacity: 0.5;
+      color: $grey;
+    }
+    &.noresize {
+      resize: none;
+    }
+  }
+}
+</style>
--- src/components/atoms/index.js
+++ src/components/atoms/index.js
@@ -1,5 +1,7 @@
 import BREAD_CRUMBS from './BreadCrumbs.vue'
 import SIDE_MENU from './SideMenu.vue'
+import INPUT_TEXT from './InputText.vue'
 
 export const BreadCrumbs = BREAD_CRUMBS
 export const SideMenu = SIDE_MENU
+export const InputText = INPUT_TEXT
