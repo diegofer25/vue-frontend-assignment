05af720b966676ace8113d6e56de64bcef5b7828
Diego Lamarão <diego.lamarao92@gmail.com>
2019-09-20 09:57:45 -0300

    created app header and footer as component

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,55 +1,39 @@
 <template>
   <div class="default-layout flex column">
-    <header class="flex row bg-primary" />
+    <app-header />
     <div class="container flex column">
       <div class="flex row">
         <bread-crumbs />
       </div>
-      <div class="flex-item grow">
+      <div class="content flex-item grow">
         <router-view />
       </div>
     </div>
-    <footer class="flex row bg-lightgrey"/>
+    <app-footer />
   </div>
 </template>
 
 <script>
 import { BreadCrumbs } from 'Components/atoms'
-
+import { AppHeader, AppFooter } from 'Components/molecules'
 export default {
   name: 'Default',
   components: {
-    BreadCrumbs
+    BreadCrumbs,
+    AppHeader,
+    AppFooter
   }
 }
 </script>
 
-<style lang="scss">
-html, body {
-  margin: 0;
-}
-</style>
-
 <style lang="scss" scoped>
-@import url('https://fonts.googleapis.com/css?family=Roboto&display=swap');
-
 .default-layout {
   position: relative;
-  font-family: 'Roboto', sans-serif;
-  header {
-    position: fixed;
-    top: 0;
-    height: 75px;
-    width: 100%;
-    z-index: 1;
-  }
   .container {
     margin-top: 75px;
     min-height: calc(100vh - 225px);
     padding: 0 10vw;
-  }
-  footer {
-    height: 150px;
+    background-color: $skyblue;
   }
 }
 </style>
