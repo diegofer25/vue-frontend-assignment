c642c18e0c181f2a12b4f79ed4b2bb6747e5635d
Diego Lamarão <diego.lamarao92@gmail.com>
2019-09-20 09:55:10 -0300

    added company mock

--- src/store/app/state.js
+++ src/store/app/state.js
@@ -1,1 +1,63 @@
-export default {}
+export default {
+  company: {
+    name: 'Microsoft',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    location: 'Redmond, WA USA',
+    imageLogo: 'squares',
+    activities: [{
+      timestamp: '',
+      image: 'content-delivery',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+    }, {
+      timestamp: '',
+      image: 'dns',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+    }, {
+      timestamp: '',
+      image: 'performance-monitoring',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+    }, {
+      timestamp: '',
+      image: 'saas',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+    }, {
+      timestamp: '',
+      image: 'traffic-management',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+    }, {
+      timestamp: '',
+      image: 'video-platform',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+    }, {
+      timestamp: '',
+      image: 'waf-d-dos-security',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+    }, {
+      timestamp: '',
+      image: 'content-delivery',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+    }],
+    similars: [{
+      image: 'logo',
+      name: 'Amazon Web Services (AWS)'
+    }, {
+      image: 'logo-copy',
+      name: 'Oracle Corporation (AWS)'
+    }],
+    spendHistory: {
+      chart: {
+        image: 'chart',
+        ext: 'svg'
+      },
+      spendLogs: [{
+        company: '',
+        value: '$500,000',
+        image: 'logo-copy-3'
+      }, {
+        company: '',
+        value: '$300,000',
+        image: 'logo-copy-2'
+      }]
+    }
+  }
+}
